9def0794272d7bd7925e5760eaf328f6754eee43
LapVQ <vuquanglap109@gmail.com>
2024-02-28 20:33:38 +0700

    modified UI cart

--- Source Code/web-client/src/component/cart/ShoppingCart.tsx
+++ Source Code/web-client/src/component/cart/ShoppingCart.tsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
-import "../../styles/ShoppingCart.scss";
 import RecommendedItems from "../RecommendedItems";
+import "../../styles/ShoppingCart.scss";
+import { useSelector } from "react-redux";
+import { formatNumber } from "../../utils/format";
 
 const ShoppingCart = () => {
   const navigate = useNavigate();
+  const carts = useSelector((state: state) => state.cartState);
+  const [totalBillAfter, setTotalBillAfter] = useState(0);
+  const [totalBill, setTotalBill] = useState(0);
+  const [deliveryFee, setDelivaryFee] = useState(30000);
 
   const handlePurchase = () => {
     navigate("/check-out");
   };
+  const goBackToShop = () => {
+    navigate("/");
+  };
+  const prepareForBill = () => {
+    console.log("check cart: ", carts);
+    let totalBill = 0;
+    carts.carts.map((item) => {
+      let total = 0;
+      total = item.quantity * (+item.price - (+item.price * item.discount / 100));
+      totalBill += total;
+    });
+    console.log("check total bill: ", totalBill);
+
+    setTotalBill(totalBill);
+    setTotalBillAfter(totalBill + deliveryFee);
+  }
+  useEffect(() => {
+    prepareForBill();
+  }, [carts.numberCart])
 
   return (
     <>
@@ -17,63 +42,82 @@ const ShoppingCart = () => {
         <div className="container">
           <div className="row">
             {/* cart */}
-            <div className="col-lg-9">
+            <div className="col-lg-8">
               <div className="card border shadow-0">
                 <div className="m-4">
-                  <h4 className="card-title mb-4">Your shopping cart</h4>
-                  <div className="row gy-3 mb-4">
-                    <div className="col-lg-5">
-                      <div className="me-lg-5">
-                        <div className="d-flex">
-                          <img
-                            src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/items/11.webp"
-                            className="border rounded me-3 img-product"
-                            alt="product"
-                          />
-                          <div className="">
-                            <NavLink to="" className="nav-link">
-                              Winter jacket for men and lady
-                            </NavLink>
-                            <p className="text-muted">Yellow, Jeans</p>
+                  <h4 className="card-title mb-4">Giỏ hàng của bạn</h4>
+                  <hr />
+
+                  {carts.numberCart > 0 ?
+                    carts.carts && carts.carts.map((product, index) => (
+                      <div className="row gy-3 mb-4">
+                        <div className="col-lg-7 col-xl-8">
+                          <div>
+                            <div className="d-flex">
+                              <img
+                                src={product.image}
+                                className="border rounded me-3 img-product"
+                                alt="product"
+                              />
+                              <div className="d-flex flex-column mt-2">
+                                <NavLink to="" className="nav-link">
+                                  <p className="fw-bold">{product.productName}</p>
+                                </NavLink>
+                                <div className="d-flex flex-row">
+                                  {product.discount ?
+                                    <>
+                                      <p className="text-muted">{formatNumber(+product.price - (+product.price * product.discount / 100))}</p>
+                                      <del className="text-muted ms-3 fw-lighter">{formatNumber(+product.price)}</del>
+                                    </>
+                                    :
+                                    <p className="text-muted">{formatNumber(+product.price)}</p>
+                                  }
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="col-lg-3 col-sm-6 col-6 d-flex flex-column maxwidth-del-btn">
+                          <div className="d-flex justify-content-center">
+                            {
+                              product.discount ?
+                                <h6>{formatNumber(product.quantity * (+product.price - (+product.price * product.discount / 100)))}</h6>
+                                :
+                                <h6>{formatNumber(product.quantity * (+product.price))}</h6>
+                            }
+                          </div>
+                          <div className="input-group border border-secondary rounded">
+                            <button className="btn btn-light border-end">
+                              <i className="fa-solid fa-minus"></i>
+                            </button>
+                            <input type="text"
+                              className="form-control border-0"
+                              aria-describedby="btnGroupQuantity"
+                              disabled
+                              value={product.quantity} />
+                            <button className="btn btn-light border-start">
+                              <i className="fa-solid fa-plus"></i>
+                            </button>
+                          </div>
+                        </div>
+                        <div className="col-lg col-sm-6 col-5 d-flex justify-content-end align-items-end align-items-lg-center">
+                          <div className="float-md-end">
+                            <button
+                              type="button"
+                              className="btn btn-outline-danger"
+                            >
+                              <i className="fa-solid fa-trash-can"></i>
+                            </button>
                           </div>
                         </div>
                       </div>
-                    </div>
-                    <div className="col-lg-2 col-sm-6 col-6 d-flex flex-row flex-lg-column flex-xl-row text-nowrap">
-                      <div className="">
-                        <select className="form-select me-4 quantity-width">
-                          <option>1</option>
-                          <option>2</option>
-                          <option>3</option>
-                          <option>4</option>
-                        </select>
-                      </div>
-                      <div className="">
-                        <text className="h6">$1156.00</text> <br />
-                        <small className="text-muted text-nowrap">
-                          $460.00 / per item
-                        </small>
-                      </div>
-                    </div>
-                    <div className="col-lg col-sm-6 d-flex justify-content-sm-center justify-content-md-start justify-content-lg-center justify-content-xl-end mb-2">
-                      <div className="float-md-end">
-                        <button
-                          type="button"
-                          className="btn btn-outline-primary mx-2"
-                        >
-                          <i className="fa-solid fa-heart text-secondary"></i>
-                        </button>
-                        <button
-                          type="button"
-                          className="btn btn-outline-danger"
-                        >
-                          <i className="fa-solid fa-trash-can"></i>
-                        </button>
-                      </div>
-                    </div>
-                  </div>
+                    ))
+                    :
+                    <h5>Giỏ hàng của bạn đang trống</h5>
+                  }
 
-                  <div className="row gy-3 mb-4">
+                  {/* demo item */}
+                  {/* <div className="row gy-3 mb-4">
                     <div className="col-lg-5">
                       <div className="me-lg-5">
                         <div className="d-flex">
@@ -101,7 +145,7 @@ const ShoppingCart = () => {
                         </select>
                       </div>
                       <div className="">
-                        <text className="h6">$44.80</text> <br />
+                        <h6>$44.80</h6> <br />
                         <small className="text-muted text-nowrap">
                           $12.20 / per item
                         </small>
@@ -123,59 +167,7 @@ const ShoppingCart = () => {
                         </button>
                       </div>
                     </div>
-                  </div>
-
-                  <div className="row gy-3">
-                    <div className="col-lg-5">
-                      <div className="me-lg-5">
-                        <div className="d-flex">
-                          <img
-                            src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/items/13.webp"
-                            className="border rounded me-3 img-product"
-                            alt="product"
-                          />
-                          <div className="">
-                            <NavLink to="" className="nav-link">
-                              Blazer Suit Dress Jacket for Men
-                            </NavLink>
-                            <p className="text-muted">XL size, Jeans, Blue</p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-lg-2 col-sm-6 col-6 d-flex flex-row flex-lg-column flex-xl-row text-nowrap">
-                      <div className="">
-                        <select className="form-select me-4 quantity-width">
-                          <option>1</option>
-                          <option>2</option>
-                          <option>3</option>
-                          <option>4</option>
-                        </select>
-                      </div>
-                      <div className="">
-                        <text className="h6">$1156.00</text> <br />
-                        <small className="text-muted text-nowrap">
-                          $460.00 / per item
-                        </small>
-                      </div>
-                    </div>
-                    <div className="col-lg col-sm-6 d-flex justify-content-sm-center justify-content-md-start justify-content-lg-center justify-content-xl-end mb-2">
-                      <div className="float-md-end">
-                        <button
-                          type="button"
-                          className="btn btn-outline-primary mx-2"
-                        >
-                          <i className="fa-solid fa-heart text-secondary"></i>
-                        </button>
-                        <button
-                          type="button"
-                          className="btn btn-outline-danger"
-                        >
-                          <i className="fa-solid fa-trash-can"></i>
-                        </button>
-                      </div>
-                    </div>
-                  </div>
+                  </div> */}
                 </div>
 
                 <div className="border-top pt-4 mx-4 mb-4">
@@ -195,7 +187,7 @@ const ShoppingCart = () => {
             {/* cart */}
 
             {/* summary */}
-            <div className="col-lg-3">
+            <div className="col-lg-4">
               <div className="card mb-3 border shadow-0">
                 <div className="card-body">
                   <form>
@@ -217,32 +209,30 @@ const ShoppingCart = () => {
               <div className="card shadow-0 border">
                 <div className="card-body">
                   <div className="d-flex justify-content-between">
-                    <p className="mb-2">Total price:</p>
-                    <p className="mb-2">$329.00</p>
+                    <p className="mb-2">Tổng tiền:</p>
+                    <p className="mb-2">{formatNumber(totalBill)}</p>
                   </div>
                   <div className="d-flex justify-content-between">
-                    <p className="mb-2">Discount:</p>
-                    <p className="mb-2 text-success">-$60.00</p>
-                  </div>
-                  <div className="d-flex justify-content-between">
-                    <p className="mb-2">TAX:</p>
-                    <p className="mb-2">$14.00</p>
+                    <p className="mb-2">Phí vận chuyển:</p>
+                    <p className="mb-2">{formatNumber(deliveryFee)}</p>
                   </div>
                   <hr />
                   <div className="d-flex justify-content-between">
-                    <p className="mb-2">Total price:</p>
-                    <p className="mb-2 fw-bold">$283.00</p>
+                    <p className="mb-2 fw-bold">Tổng tiền:</p>
+                    <p className="mb-2 fw-bold text-danger fs-4">{formatNumber(totalBillAfter)}</p>
                   </div>
 
                   <div className="mt-3">
                     <button
-                      className="btn btn-success w-100 shadow-0 mb-2"
+                      className="btn btn-danger w-100 shadow-0 mb-2 py-3"
                       onClick={() => handlePurchase()}
                     >
-                      Make Purchase
+                      Thanh toán
                     </button>
-                    <button className="btn btn-light w-100 border mt-2">
-                      Back to shop
+                    <button
+                      className="btn btn-light w-100 border mt-2"
+                      onClick={goBackToShop}>
+                      Tiếp tục mua hàng
                     </button>
                   </div>
                 </div>
